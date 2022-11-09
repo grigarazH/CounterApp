3de1037d9ad01ede2e2243ed485db4d5ccb513d8
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-11-09 20:05:58 +0300

    feat: realized sprint 3 tasks

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter: Int = 0 {
+        didSet {
+            counterLabel.text = "Значение счётчика: \(counter)"
+        }
+    }
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func counterButtonTouch(_ sender: Any) {
+        counter += 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
